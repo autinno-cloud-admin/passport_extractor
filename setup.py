8feb9f9c6e6196bbbe319ba153b41803612c74d0
autinno-cloud-admin <cloud.admin@autinno.com>
2026-07-27 12:21:02 +0000

    fix: added setup.py, pyproject.tomal

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="passport_extractor",
+    version="0.0.1",
+    description="Extract passport details from passport MRZ code",
+    packages=["passport_extractor"],
+)
